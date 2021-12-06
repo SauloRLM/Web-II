87eff4ab1e4f7011f6801768c606fd6db532794c
SauloRLM <saulo2000ramon@gmail.com>
2021-12-05 21:45:26 -0700

    05-12-2021

---
+++ my-components/projects/ui-controls/src/lib/copy-button/copy-button.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'lib-copy-button',
+  templateUrl: './copy-button.component.html',
+  styleUrls: ['./copy-button.component.css']
+})
+export class CopyButtonComponent implements OnInit {
+@Input() data = '';
+@Output() copied = new EventEmitter();
+  constructor() { }
+
+  onCopy(){
+    this.copied.next();
+  }
+  ngOnInit(): void {
+  }
+
+}
